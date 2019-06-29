e4aea3694b5a6579e6f14abc9bc52e0faa5ee4b1
Станислав Быков <stanislav.byckov@gmail.com>
2019-06-29 10:38:05 +0300

    Добавьте файлы проекта.

---
+++ UnitTest/UnitTest.cs
@@ -0,0 +1,67 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace UnitTest
+{
+	[TestClass]
+	public class UnitTest
+	{
+		public IWebDriver Driver { get; set; }
+
+		[TestInitialize]
+		public void SetupTest()
+		{
+			Driver = new ChromeDriver();
+		}
+
+		[TestCleanup]
+		public void TeardownTest()
+		{
+			this.Driver.Quit();
+		}
+
+		[TestMethod]
+		[System.Obsolete]
+		public void TestCase1()
+		{
+			YandexMarketPage yandexMarketPage = new YandexMarketPage(Driver);
+			string section = "Телевизоры";
+			string startingPrice = "20000";
+			string[] manufacturersList =
+			{
+				"Samsung",
+				"LG"
+			};
+
+			yandexMarketPage.Navigate();
+			yandexMarketPage.Paragraph1();
+			yandexMarketPage.Paragraph2(section, startingPrice, manufacturersList);
+
+			string resultTitle = yandexMarketPage.Paragraph3();
+
+			yandexMarketPage.VolidateResults(resultTitle);
+		}
+
+		[TestMethod]
+		[System.Obsolete]
+		public void TestCase2()
+		{
+			YandexMarketPage yandexMarketPage = new YandexMarketPage(Driver);
+			string section = "Наушники";
+			string startingPrice = "5000";
+			string[] manufacturersList =
+			{
+				"Beats"
+			};
+
+			yandexMarketPage.Navigate();
+			yandexMarketPage.Paragraph1();
+			yandexMarketPage.Paragraph2(section, startingPrice, manufacturersList);
+
+			string resultTitle = yandexMarketPage.Paragraph3();
+
+			yandexMarketPage.VolidateResults(resultTitle);
+		}
+	}
+}
